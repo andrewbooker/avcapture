7c547193315df2d2af8de9caa87ee6218c884833
Andrew Booker <improvizone@gmail.com>
2020-04-07 15:17:45 +0100

    simple script to capture video using open cv

---
+++ video.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+import cv2
+import os
+
+class RecordVideo():
+	def __init__(self, fqfn, deviceIdx):
+		self.fn = fqfn
+		self.deviceIdx = deviceIdx
+	
+	def start(self, shouldStop):
+		fourcc = cv2.VideoWriter_fourcc(*'XVID')
+		cap = cv2.VideoCapture(self.deviceIdx)
+		out = cv2.VideoWriter(self.fn, fourcc, 30.0, (640, 480))
+
+		while cap.isOpened() and not shouldStop.is_set():
+			ret, frame = cap.read()
+			if ret==True:
+				out.write(frame)
+			else:
+				break
+				
+		print("stopping video %d" % self.deviceIdx)
+		out.release()
+		cap.release()
+		
+		
+		
+import threading
+import keyboard
+shouldStop = threading.Event()
+
+
+recorders = []
+recorders.append(RecordVideo(os.path.join(".", "video0.avi"), 0))
+
+threads = [threading.Thread(target=r.start, args=(shouldStop,), daemon=True) for r in recorders]
+
+print ("Starting recording. Press 'q' to stop.")
+for t in threads:
+	t.start()
+
+keyboard.wait("q")
+print ("stopping...")
+shouldStop.set()
+
+for t in threads:
+	t.join()
+print("done")
+
