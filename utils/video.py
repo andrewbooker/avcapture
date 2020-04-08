2b328832b741d90720b5cea0a63cca61ebb84f7c
Andrew Booker <improvizone@gmail.com>
2020-04-08 17:05:12 +0100

    renaming for consistency

--- utils/video.py
+++ utils/video.py
@@ -4,14 +4,14 @@ import cv2
 
 class RecordVideo():
 	def __init__(self, fqfn, deviceIdx, fps):
-		self.fn = fqfn
+		self.fqfn = fqfn
 		self.deviceIdx = deviceIdx
 		self.fps = fps
 	
 	def start(self, shouldStop):
 		fourcc = cv2.VideoWriter_fourcc(*'XVID')
 		cap = cv2.VideoCapture(self.deviceIdx)
-		out = cv2.VideoWriter(self.fn, fourcc, float(self.fps), (640, 480))
+		out = cv2.VideoWriter(self.fqfn, fourcc, float(self.fps), (640, 480))
 
 		while cap.isOpened() and not shouldStop.is_set():
 			ret, frame = cap.read()
